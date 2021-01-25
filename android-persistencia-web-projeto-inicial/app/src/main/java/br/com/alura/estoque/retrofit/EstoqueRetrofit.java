403909cfb8d75f54e560671b37a5b4817f6d270f
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-01-25 13:59:49 -0300

    Adição do Retrofit

--- android-persistencia-web-projeto-inicial/app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ android-persistencia-web-projeto-inicial/app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -1,15 +1,25 @@
 package br.com.alura.estoque.retrofit;
 
 import br.com.alura.estoque.retrofit.service.ProdutoService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EstoqueRetrofit {
 
     private final ProdutoService produtoService;
 
     public EstoqueRetrofit() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://192.168.0.104:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
         produtoService = retrofit.create(ProdutoService.class);
     }
