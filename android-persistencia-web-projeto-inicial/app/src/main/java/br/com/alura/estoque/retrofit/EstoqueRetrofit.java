391d7319bbe92667dbd62dd46a89d9dc8ed83eca
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-01-25 10:43:03 -0300

    Configurando o Service

--- android-persistencia-web-projeto-inicial/app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ android-persistencia-web-projeto-inicial/app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -1,12 +1,20 @@
 package br.com.alura.estoque.retrofit;
 
+import br.com.alura.estoque.retrofit.service.ProdutoService;
 import retrofit2.Retrofit;
 
 public class EstoqueRetrofit {
 
+    private final ProdutoService produtoService;
+
     public EstoqueRetrofit() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://localhost:8080/")
+                .baseUrl("http://192.168.0.104:8080/")
                 .build();
+        produtoService = retrofit.create(ProdutoService.class);
+    }
+
+    public ProdutoService getProdutoService() {
+        return produtoService;
     }
 }
