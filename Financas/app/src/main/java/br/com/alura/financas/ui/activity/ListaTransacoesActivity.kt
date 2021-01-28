d43e55ffb3941ce8175b1a4ca08d81f473137747
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-01-28 12:19:35 -0300

    Implementação do Adapter

--- Financas/app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
+++ Financas/app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
@@ -4,19 +4,22 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import br.com.alura.financas.R
+import br.com.alura.financas.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?): Unit {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf("Comida - R$ 20,50", "Economia - R$ 100,00")
+        val transacoes = listOf("Comida - R$ 20,50",
+                "Economia - R$ 100,00")
 
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+        val arrayAdapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, transacoes)
 
-        lista_transacoes_listview.adapter
-
+        lista_transacoes_listview.setAdapter(ListaTransacoesAdapter())
     }
+
 }
