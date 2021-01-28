6f061cda668e13f410cecb0f27155a32b453cf49
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-01-28 09:49:10 -0300

    Criando primeira tela do App

---
+++ Financas/app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,14 @@
+package br.com.alura.financas.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.alura.financas.R
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) : Unit {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+
+}
