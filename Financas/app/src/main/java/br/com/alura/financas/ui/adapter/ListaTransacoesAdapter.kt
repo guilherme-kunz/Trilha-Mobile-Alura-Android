93c5fd6b869d95e843882e294fb7b14cc3e0b9cc
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-01-28 14:03:09 -0300

    Implementação do Adapter

--- Financas/app/src/main/java/br/com/alura/financas/ui/adapter/ListaTransacoesAdapter.kt
+++ Financas/app/src/main/java/br/com/alura/financas/ui/adapter/ListaTransacoesAdapter.kt
@@ -1,23 +1,31 @@
 package br.com.alura.financas.ui.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import br.com.alura.financas.R
 
-class ListaTransacoesAdapter : BaseAdapter () {
+class ListaTransacoesAdapter (transacoes : List<String>,
+                              context : Context) : BaseAdapter () {
+
+    private val transacoes = transacoes
+    private val context = context
+
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return transacoes.size
     }
 
-    override fun getItem(p0: Int): Any {
-        TODO("Not yet implemented")
+    override fun getItem(posicao: Int): String {
+        return transacoes[posicao]
     }
 
     override fun getItemId(p0: Int): Long {
-        TODO("Not yet implemented")
+        return 0
     }
 
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        TODO("Not yet implemented")
+    override fun getView(posicao: Int, view: View?, parent: ViewGroup?): View {
+        return LayoutInflater.from(context).inflate(R.layout.transacao_item, parent, false)
     }
 }
