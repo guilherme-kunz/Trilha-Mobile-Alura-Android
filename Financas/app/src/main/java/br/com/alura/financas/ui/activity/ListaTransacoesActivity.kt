ae827b4b49937f13a9ae09bbda61789f4f369977
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-01-28 12:02:03 -0300

    Implementação do Synthetic

--- Financas/app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
+++ Financas/app/src/main/java/br/com/alura/financas/ui/activity/ListaTransacoesActivity.kt
@@ -2,13 +2,21 @@ package br.com.alura.financas.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import br.com.alura.financas.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) : Unit {
+    override fun onCreate(savedInstanceState: Bundle?): Unit {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
-    }
 
+        val transacoes = listOf("Comida - R$ 20,50", "Economia - R$ 100,00")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+
+        lista_transacoes_listview.adapter
+
+    }
 }
