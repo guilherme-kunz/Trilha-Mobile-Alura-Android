d0d2748d805dcfb14819441dd7900b2c3c488212
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-01-29 16:05:06 -0300

    Adição de cores nas transações

--- FinancasK/app/src/main/java/br/com/alura/financask/ui/adapter/ListaTransacoesAdapter.kt
+++ FinancasK/app/src/main/java/br/com/alura/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import androidx.core.content.ContextCompat
 import br.com.alura.financask.R
+import br.com.alura.financask.model.Tipo
 import br.com.alura.financask.model.Transacao
 import kotlinx.android.synthetic.main.transacao_item.view.*
 import java.text.SimpleDateFormat
@@ -22,6 +24,18 @@ class ListaTransacoesAdapter(transacoes: List<Transacao>,
             .inflate(R.layout.transacao_item, parent, false)
 
         val transacao = transacoes[posicao]
+
+        if (transacao.tipo == Tipo.RECEITA){
+            viewCriada.transacao_valor.setTextColor(ContextCompat.getColor(context, R.color.receita))
+        } else {
+            viewCriada.transacao_valor.setTextColor((ContextCompat.getColor(context, R.color.despesa)))
+        }
+
+        if (transacao.tipo == Tipo.RECEITA){
+            viewCriada.transacao_icone.setBackgroundResource(R.drawable.icone_transacao_item_receita)
+        } else {
+            viewCriada.transacao_icone.setBackgroundResource((R.drawable.icone_transacao_item_despesa))
+        }
 
         viewCriada.transacao_valor.text = transacao.valor.toString()
         viewCriada.transacao_categoria.text = transacao.categoria
