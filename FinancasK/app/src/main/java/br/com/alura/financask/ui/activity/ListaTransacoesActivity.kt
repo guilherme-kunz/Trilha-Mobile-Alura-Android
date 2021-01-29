3963d22aad4bf46373ede2ea1e6fd4779c35ec73
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-01-29 15:41:50 -0300

    Atualização dos construtores

--- FinancasK/app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ FinancasK/app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -3,11 +3,11 @@ package br.com.alura.financask.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.financask.R
+import br.com.alura.financask.model.Tipo
 import br.com.alura.financask.model.Transacao
 import br.com.alura.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import java.math.BigDecimal
-import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
@@ -15,10 +15,9 @@ class ListaTransacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf(Transacao(BigDecimal(20.5),
-            "Comida", Calendar.getInstance()),
-            Transacao(BigDecimal(100.0), "Economia",
-                Calendar.getInstance()))
+        val transacoes = listOf(Transacao(valor = BigDecimal(20.5),
+            tipo = Tipo.DESPESA, data = java.util.Calendar.getInstance()),
+            Transacao(BigDecimal(100.0), categoria = "Economia", tipo = Tipo.RECEITA))
 
         lista_transacoes_listview.setAdapter(
             ListaTransacoesAdapter(transacoes, this))
