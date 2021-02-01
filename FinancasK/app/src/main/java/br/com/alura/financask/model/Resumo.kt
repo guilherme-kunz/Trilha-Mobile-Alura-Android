b91885ccf1bd6a80f7029e87655c0cd876460944
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-02-01 16:24:36 -0300

    Funções de collections e expressão lambda

--- FinancasK/app/src/main/java/br/com/alura/financask/model/Resumo.kt
+++ FinancasK/app/src/main/java/br/com/alura/financask/model/Resumo.kt
@@ -4,28 +4,26 @@ import java.math.BigDecimal
 
 class Resumo (private val transacoes : List<Transacao>) {
 
-    fun receita() : BigDecimal{
-        var totalReceita = BigDecimal.ZERO
-        for (transacao in transacoes) {
-            if (transacao.tipo == Tipo.RECEITA) {
-                totalReceita = totalReceita.plus(transacao.valor)
+    fun receita() : BigDecimal {
+        val somaDeReceitas: Double = transacoes
+            .filter { transacao -> transacao.tipo == Tipo.RECEITA }
+            .sumByDouble { transacao -> transacao.valor.toDouble() }
+
+            return BigDecimal(somaDeReceitas)
+    }
+
+        fun despesa(): BigDecimal {
+            var totalDespesa = BigDecimal.ZERO
+            for (transacao in transacoes) {
+                if (transacao.tipo == Tipo.DESPESA) {
+                    totalDespesa = totalDespesa.plus(transacao.valor)
+                }
             }
+
+            return totalDespesa
         }
-        return totalReceita
-    }
 
-    fun despesa() : BigDecimal {
-        var totalDespesa = BigDecimal.ZERO
-        for (transacao in transacoes) {
-            if (transacao.tipo == Tipo.DESPESA) {
-                totalDespesa = totalDespesa.plus(transacao.valor)
-            }
+        fun total(): BigDecimal {
+            return receita().subtract(despesa())
         }
-        return totalDespesa
-    }
-
-    fun total () : BigDecimal {
-       return receita().subtract(despesa())
-    }
-
 }
