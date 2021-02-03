76162cd4149503d704ad192f91584cabe812d0c6
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-02-03 14:52:09 -0300

    Implementação da herança

--- FinancasK/app/src/main/java/br/com/alura/financask/ui/dialog/FormularioTransacaoDialog.kt
+++ FinancasK/app/src/main/java/br/com/alura/financask/ui/dialog/FormularioTransacaoDialog.kt
@@ -4,11 +4,13 @@ import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import br.com.alura.financask.R
-import br.com.alura.financask.extensions.converteParaCalendar
+import br.com.alura.financask.delegate.TransacaoDelegate
+import br.com.alura.financask.extension.converteParaCalendar
 import br.com.alura.financask.extensions.formataParaBrasileiro
 import br.com.alura.financask.model.Tipo
 import br.com.alura.financask.model.Transacao
@@ -16,48 +18,52 @@ import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
 import java.util.*
 
-abstract class FormularioTransacaoDialog(
-        private val context: Context,
-        private val viewGroup: ViewGroup) {
+open class FormularioTransacaoDialog(private val context: Context,
+                                     private val viewGroup: ViewGroup?) {
 
     private val viewCriada = criaLayout()
-    protected val campoValor = viewCriada.form_transacao_valor
-    protected val campoData = viewCriada.form_transacao_data
-    protected val campoCategoria = viewCriada.form_transacao_categoria
-    protected abstract val tituloBotaoPositivo: String
+    private val campoValor = viewCriada.form_transacao_valor
+    private val campoCategoria = viewCriada.form_transacao_categoria
+    private val campoData = viewCriada.form_transacao_data
 
-    fun chama(tipo: Tipo,
-              delegate: (transacao: Transacao) -> Unit) {
+    fun chama(tipo: Tipo, transacaoDelegate: TransacaoDelegate) {
         configuraCampoData()
         configuraCampoCategoria(tipo)
-        configuraFormulario(tipo, delegate)
+        configuraFormulario(tipo, transacaoDelegate)
     }
 
-    private fun configuraFormulario(tipo: Tipo,
-                                    delegate: (transacao: Transacao) -> Unit) {
-
+    private fun configuraFormulario(tipo: Tipo, transacaoDelegate: TransacaoDelegate) {
         val titulo = tituloPor(tipo)
-
         AlertDialog.Builder(context)
-                .setTitle(titulo)
-                .setView(viewCriada)
-                .setPositiveButton(tituloBotaoPositivo) { _, _ ->
-                    val valorEmTexto = campoValor.text.toString()
-                    val dataEmTexto = campoData.text.toString()
-                    val categoriaEmTexto = campoCategoria.selectedItem.toString()
+            .setTitle(titulo)
+            .setView(viewCriada)
+            .setPositiveButton("Adicionar"
+            ) { _, _ ->
+                val valorEmTexto = campoValor.text.toString()
+                val dataEmTexto = campoData.text.toString()
+                val categoriaEmTexto = campoCategoria.selectedItem.toString()
 
-                    val valor = converteCampoValor(valorEmTexto)
-                    val data = dataEmTexto.converteParaCalendar()
+                val valor = converteCampoValor(valorEmTexto)
 
-                    val transacaoCriada = Transacao(tipo = tipo,
-                            valor = valor,
-                            data = data,
-                            categoria = categoriaEmTexto)
+                val data = dataEmTexto.converteParaCalendar()
 
-                    delegate(transacaoCriada)
-                }
-                .setNegativeButton("Cancelar", null)
-                .show()
+                val transacaoCriada = Transacao(
+                    tipo = tipo,
+                    valor = valor,
+                    data = data,
+                    categoria = categoriaEmTexto
+                )
+
+                transacaoDelegate.delegate(transacaoCriada)
+            }
+            .setNegativeButton("Cancelar", null)
+            .show()
+    }
+
+    private fun tituloPor(tipo: Tipo) = if (tipo == Tipo.RECEITA) {
+        R.string.adiciona_receita
+    } else {
+        R.string.adiciona_despesa
     }
 
     private fun converteCampoValor(valorEmTexto: String): BigDecimal {
@@ -65,17 +71,22 @@ abstract class FormularioTransacaoDialog(
             BigDecimal(valorEmTexto)
         } catch (exception: NumberFormatException) {
             Toast.makeText(context,
-                    "Falha na conversão de valor",
-                    Toast.LENGTH_LONG).show()
+                "Falha na conversão de valor",
+                Toast.LENGTH_LONG)
+                .show()
             BigDecimal.ZERO
         }
     }
 
     private fun configuraCampoCategoria(tipo: Tipo) {
-        val categorias = categoriasPor(tipo)
-
-        val adapter = ArrayAdapter.createFromResource(
-                context, categorias,
+        val categorias = if(tipo == Tipo.RECEITA){
+            R.array.categorias_de_receita
+        } else {
+            R.array.categorias_de_despesa
+        }
+        val adapter = ArrayAdapter
+            .createFromResource(context,
+               categorias,
                 android.R.layout.simple_spinner_dropdown_item)
 
         campoCategoria.adapter = adapter
@@ -90,25 +101,21 @@ abstract class FormularioTransacaoDialog(
 
         campoData.setText(hoje.formataParaBrasileiro())
         campoData.setOnClickListener {
-            DatePickerDialog(context,
-                    { _, ano, mes, dia ->
+                DatePickerDialog(context,
+                     { _, ano, mes, dia ->
                         val dataSelecionada = Calendar.getInstance()
                         dataSelecionada.set(ano, mes, dia)
-                        campoData.setText(dataSelecionada
-                                .formataParaBrasileiro())
-                    },
-                    ano, mes, dia)
+                        campoData.setText(dataSelecionada.formataParaBrasileiro())
+                    }
+                    , ano, mes, dia)
                     .show()
-        }
+            }
     }
 
-    protected fun categoriasPor(tipo: Tipo) = if (tipo == Tipo.RECEITA)
-        R.array.categorias_de_receita else R.array.categorias_de_despesa
-
-    protected abstract fun tituloPor(tipo: Tipo): Int
-
-    private fun criaLayout() = LayoutInflater.from(context)
+    private fun criaLayout(): View {
+        return LayoutInflater.from(context)
             .inflate(R.layout.form_transacao,
-                    viewGroup,
-                    false)
+                viewGroup,
+                false)
+    }
 }
