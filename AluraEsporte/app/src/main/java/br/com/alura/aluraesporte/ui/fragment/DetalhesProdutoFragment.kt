4e2348cf3360e300e792dd27e38ecd5f85dad74d
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-02-16 09:29:56 -0300

    Reutilizando código com herança

--- AluraEsporte/app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
+++ AluraEsporte/app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
@@ -9,12 +9,11 @@ import androidx.navigation.fragment.navArgs
 import br.com.alura.aluraesporte.R
 import br.com.alura.aluraesporte.extensions.formatParaMoedaBrasileira
 import br.com.alura.aluraesporte.ui.viewmodel.DetalhesProdutoViewModel
-import br.com.alura.aluraesporte.ui.viewmodel.LoginViewModel
 import kotlinx.android.synthetic.main.detalhes_produto.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
-class DetalhesProdutoFragment : Fragment() {
+class DetalhesProdutoFragment : BaseFragment() {
 
     private val argumentos by navArgs<DetalhesProdutoFragmentArgs>()
 
@@ -22,14 +21,9 @@ class DetalhesProdutoFragment : Fragment() {
         argumentos.produtoId
     }
     private val viewModel: DetalhesProdutoViewModel by viewModel { parametersOf(produtoId) }
-    private val loginViewModel: LoginViewModel by viewModel()
+
     private val controlador by lazy {
         findNavController()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -48,25 +42,6 @@ class DetalhesProdutoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         buscaProduto()
         configuraBotaoComprar()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater?.inflate(R.menu.menu_lista_produtos, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item?.itemId == R.id.menu_lista_produtos_deslogar) {
-            loginViewModel.desloga()
-            vaiParaLogin()
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun vaiParaLogin() {
-        val directions =
-            DetalhesProdutoFragmentDirections.acaoGlobalLogin()
-        controlador.navigate(directions)
     }
 
     private fun configuraBotaoComprar() {
