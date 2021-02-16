789064d183f5ae2112070a344d48081b0c7cb2d0
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-02-16 16:19:42 -0300

    Apresentando mensagens de erros específicos

---
+++ Firebase/app/src/main/java/br/com/alura/aluraesporte/repository/FirebaseAuthRepository.kt
@@ -0,0 +1,51 @@
+package br.com.alura.aluraesporte.repository
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.*
+
+private const val TAG ="FirebaseAuthRepository"
+
+class FirebaseAuthRepository(private val firebaseAuth: FirebaseAuth) {
+
+    private fun desloga(firebaseAuth: FirebaseAuth) {
+        firebaseAuth.signOut()
+    }
+
+    private fun verificaUsuario(firebaseAuth: FirebaseAuth) {
+        val usuarioFirebase: FirebaseUser? = firebaseAuth.currentUser
+        if (usuarioFirebase != null) {
+        } else {
+        }
+    }
+
+    private fun autenticaUsuario(firebaseAuth: FirebaseAuth) {
+        firebaseAuth.signInWithEmailAndPassword("gui-3@aluraesporte.com", "teste123")
+            .addOnSuccessListener {
+            }.addOnFailureListener {
+            }
+    }
+
+     fun cadastra(email: String, senha: String): LiveData<Resource<Boolean>> {
+        val liveData = MutableLiveData<Resource<Boolean>>()
+        val tarefa =
+            firebaseAuth.createUserWithEmailAndPassword(email, senha)
+        tarefa.addOnSuccessListener {
+            Log.i(TAG, "cadastra: cadastro sucedido")
+            liveData.value = Resource(true)
+        }
+        tarefa.addOnFailureListener {exception ->
+            Log.e(TAG, "cadastra: cadastro falhou", exception)
+            val mensagemErro: String = when(exception) {
+                is FirebaseAuthWeakPasswordException -> "Senha precisa de pelo menos 6 dígitos"
+                is FirebaseAuthInvalidCredentialsException -> "E-mail inválido"
+                is FirebaseAuthUserCollisionException -> "E-mail já cadastrado"
+                else -> "Erro desconhecido"
+            }
+            liveData.value = Resource(false, mensagemErro)
+        }
+         return liveData
+    }
+
+}
