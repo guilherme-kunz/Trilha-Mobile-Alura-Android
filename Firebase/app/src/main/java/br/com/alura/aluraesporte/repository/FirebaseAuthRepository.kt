c07a42c6e63149a006a8eb6678b78a155565f313
Guilherme Butzen Kunz <guilhermebkunz@aluno.santoangelo.uri.br>
2021-02-17 09:48:08 -0300

    Integrando verificação do usuário com Firebase

--- Firebase/app/src/main/java/br/com/alura/aluraesporte/repository/FirebaseAuthRepository.kt
+++ Firebase/app/src/main/java/br/com/alura/aluraesporte/repository/FirebaseAuthRepository.kt
@@ -3,22 +3,17 @@ package br.com.alura.aluraesporte.repository
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import br.com.alura.aluraesporte.model.Usuario
 import com.google.firebase.auth.*
+import java.lang.Exception
 import java.lang.IllegalArgumentException
 
 private const val TAG ="FirebaseAuthRepository"
 
 class FirebaseAuthRepository(private val firebaseAuth: FirebaseAuth) {
 
-    private fun desloga(firebaseAuth: FirebaseAuth) {
+    fun desloga() {
         firebaseAuth.signOut()
-    }
-
-    private fun verificaUsuario(firebaseAuth: FirebaseAuth) {
-        val usuarioFirebase: FirebaseUser? = firebaseAuth.currentUser
-        if (usuarioFirebase != null) {
-        } else {
-        }
     }
 
     private fun autenticaUsuario(firebaseAuth: FirebaseAuth) {
@@ -28,29 +23,42 @@ class FirebaseAuthRepository(private val firebaseAuth: FirebaseAuth) {
             }
     }
 
-     fun cadastra(email: String, senha: String): LiveData<Resource<Boolean>> {
+     fun cadastra(usuario: Usuario): LiveData<Resource<Boolean>> {
         val liveData = MutableLiveData<Resource<Boolean>>()
          try {
              val tarefa =
-                 firebaseAuth.createUserWithEmailAndPassword(email, senha)
+                 firebaseAuth.createUserWithEmailAndPassword(usuario.email, usuario.senha)
              tarefa.addOnSuccessListener {
                  Log.i(TAG, "cadastra: cadastro sucedido")
                  liveData.value = Resource(true)
              }
              tarefa.addOnFailureListener {exception ->
                  Log.e(TAG, "cadastra: cadastro falhou", exception)
-                 val mensagemErro: String = when(exception) {
-                     is FirebaseAuthWeakPasswordException -> "Senha precisa de pelo menos 6 dígitos"
-                     is FirebaseAuthInvalidCredentialsException -> "E-mail inválido"
-                     is FirebaseAuthUserCollisionException -> "E-mail já cadastrado"
-                     else -> "Erro desconhecido"
-                 }
+                 val mensagemErro: String = devolveErroDeCadastro(exception)
                  liveData.value = Resource(false, mensagemErro)
              }
          } catch (e: IllegalArgumentException) {
              liveData.value = Resource(false, "E-mail ou senha não pode ser vazio")
          }
          return liveData
+    }
+
+    private fun devolveErroDeCadastro(exception: Exception): String {
+        return when (exception) {
+            is FirebaseAuthWeakPasswordException -> "Senha precisa de pelo menos 6 dígitos"
+            is FirebaseAuthInvalidCredentialsException -> "E-mail inválido"
+            is FirebaseAuthUserCollisionException -> "E-mail já cadastrado"
+            else -> "Erro desconhecido"
+        }
+    }
+
+    fun estaLogado(): Boolean {
+        val usuarioFirebase: FirebaseUser? = firebaseAuth.currentUser
+        if (usuarioFirebase != null) {
+            return true
+        }
+            return false
+
     }
 
 }
